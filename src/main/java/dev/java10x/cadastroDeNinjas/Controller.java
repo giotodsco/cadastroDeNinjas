a20de19866bf5b370792f2a35feb320a5209f70e
giotodsco <todescogiovanni18@gmail.com>
2025-05-10 17:24:42 -0300

    Criado o controller

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/Controller.java
@@ -0,0 +1,22 @@
+package dev.java10x.cadastroDeNinjas;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class Controller {
+
+    @GetMapping("/rotaUm")
+    public String rotaUm(){
+        return "Olá, rota 1";
+    }
+
+    @GetMapping("/rotaDois")
+    public String rotaDois(){
+        return "Olá, rota 2";
+    }
+}
